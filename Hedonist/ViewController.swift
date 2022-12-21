aec7626d7745953d65244aeec359d429e646b146
Alexandr <a.lobanov@start.ru>
2022-12-21 20:59:38 +0300

    Project setup & optimization settings

---
+++ Hedonist/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Hedonist
+//
+//  Created by a.lobanov on 12/21/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
